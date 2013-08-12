2f5e523f5a03691a57c8ae8309ceaf07fdbdc678
Jonathan Calvert <jcalvert@taximagic.com>
2013-08-12 11:08:43 -0400

    More sensible percentage

--- app/models/coverage_report.rb
+++ app/models/coverage_report.rb
@@ -2,6 +2,6 @@ class CoverageReport < ActiveRecord::Base
   attr_accessible :build_id, :coverage, :publication_date, :repo, :sha, :key
 
   def derivation
-  	self.coverage - CoverageReport.average('coverage')
+  	(self.coverage - CoverageReport.average('coverage')).round(3)*100
   end
 end
